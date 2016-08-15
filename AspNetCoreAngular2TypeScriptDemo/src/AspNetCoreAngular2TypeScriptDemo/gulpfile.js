220f4cd1063c1570aae7fe8cd4e63f53d8748fb4
Jaliya Udagedara <jaliya.udagedara@hotmail.com>
2016-08-15 12:22:25 +0530

    Initial commit.

---
+++ AspNetCoreAngular2TypeScriptDemo/src/AspNetCoreAngular2TypeScriptDemo/gulpfile.js
@@ -0,0 +1,14 @@
+﻿var gulp = require('gulp');
+var del = require('del');
+
+var paths = {
+    nodeModules: 'node_modules/**/*.js'
+};
+
+gulp.task('clean', function () {
+    return del(['wwwroot/scripts/**/*']);
+});
+
+gulp.task('build', function () {
+    gulp.src(paths.nodeModules).pipe(gulp.dest('wwwroot/scripts'))
+});
---
+++ AspNetCoreAngular2TypeScriptDemo/src/AspNetCoreAngular2TypeScriptDemo/wwwroot/app/app.component.ts
@@ -0,0 +1,6 @@
+﻿import { Component } from '@angular/core';
+@Component({
+    selector: 'my-app',
+    template: '<h1>Welcome to Angular 2 with TypeScript</h1>'
+})
+export class AppComponent { }
